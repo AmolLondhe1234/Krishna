9e676d2befc2035e6a252e12d6061ab5fe5c0cbc
amol londhe <amolbrand00@gmail.com>
2024-01-10 00:40:29 +0530

    aa

--- src/setup.sh
+++ src/setup.sh
@@ -2,8 +2,6 @@
 
 # Install requirements from requirements.txt
 pip install --upgrade pip
-pip install httpcore==0.9.1
-pip install httpx==0.13.3
 pip install -r requirements.txt
-pip install --upgrade httpcore
-
+pip install --upgrade httpcore httpx
+pip install googletrans==4.0.0-rc1
