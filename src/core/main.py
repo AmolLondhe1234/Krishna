c9484d0ce9b0a066f83e857df15e9d39e2644367
amol londhe <amolbrand00@gmail.com>
2024-01-29 20:59:50 +0530

    asd

--- src/core/main.py
+++ src/core/main.py
@@ -17,7 +17,6 @@ class RecoBotModelGenerator(MongoService):
 
     def __init__(self) -> None:
         super().__init__()
-        # os.environ["OPENAI_API_KEY"] = self.cfg.get('openapi','key')
 
     def read_files(self):
         documents = []
@@ -34,7 +33,7 @@ class RecoBotModelGenerator(MongoService):
         collection = self.db['embd']
         text_splitter = CharacterTextSplitter(chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
         documents = text_splitter.split_documents(documents)
-        self.drop_collection_and_index(collection, EMBD_INDEX) # remove everything from db and atlas
+        self.drop_collection_and_index(collection, EMBD_INDEX)
         MongoDBAtlasVectorSearch.from_documents(
             documents, embedding=OpenAIEmbeddings(), collection=collection, index_name=EMBD_INDEX
         )
@@ -46,10 +45,7 @@ class QARecoBot(MongoService):
 
     def __init__(self) -> None:
         super().__init__()
-        # os.environ["OPENAI_API_KEY"] = self.cfg.get('openapi','key')
         self.translator = Translator()
-
-        # Use 'translate' library for language translation
         self.qa_chain = self.chain()
 
     def chain(self):
@@ -57,38 +53,31 @@ class QARecoBot(MongoService):
         vectordb =  MongoDBAtlasVectorSearch(collection=self.embd_collection_name, embedding=OpenAIEmbeddings(), index_name=EMBD_INDEX)
         chat_qa = ConversationalRetrievalChain.from_llm(
             ChatOpenAI(temperature=TEMPERATURE, model_name=MODEL),
-            vectordb.as_retriever(search_kwargs={"k":20, "case_sensetive":False}),
+            vectordb.as_retriever(search_kwargs={"case_sensetive":False}),
             return_source_documents=True,
             verbose=False
         )
         return chat_qa   
     
     def detect_language(self, text):
-        # Detect the language of the input text
         lang = self.translator.detect(text).lang
         return lang
 
     def translate_to_english(self, text):
-        # Translate text to English
         translation = self.translator.translate(text, dest='en')
         return translation.text
     
     def interact(self, question, user_language=None):
-        # If user_language is not provided, detect the language
         if not user_language:
             user_language = self.detect_language(question)
 
-        # Translate the question to English
         if user_language != 'en':
             question = self.translate_to_english(question)
 
-        # Construct the prompt
         prompt = HEADER + f"\nQuestion: {question}"
 
-        # Call your QA method with the translated question
         result = self.qa_chain({"question": prompt, "chat_history": ""})
 
-        # If the answer is in English, translate it back to the user's language
         if user_language != 'en':
             result["answer"] = self.translator.translate(result["answer"], dest=user_language).text
 
