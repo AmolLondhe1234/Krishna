8827b8d7139c8b238b8a2639dede8e8e48125c87
amol londhe <amolbrand00@gmail.com>
2024-02-07 17:38:34 +0530

    jk

--- src/core/main.py
+++ src/core/main.py
@@ -12,14 +12,15 @@ from langchain.document_loaders import PyPDFLoader
 from tqdm import tqdm
 from database.mongoservices import MongoService
 from googletrans import Translator
-
+from multiprocessing import Pool
 EMBD_INDEX = "langchain_demo"
 
 class RecoBotModelGenerator(MongoService):
     def __init__(self) -> None:
         super().__init__()
-        # os.environ["OPENAI_API_KEY"] = self.cfg.get('openapi', 'key')
+        os.environ["OPENAI_API_KEY"] = self.cfg.get('openapi', 'key')
 
+    
     def load_document(self, file):
         pdf_path = DATA_PATH + "/" + file
         loader = PyPDFLoader(pdf_path)
@@ -51,21 +52,21 @@ class RecoBotModelGenerator(MongoService):
 class QARecoBot(MongoService):
     def __init__(self) -> None:
         super().__init__()
-        # os.environ["OPENAI_API_KEY"] = self.cfg.get('openapi', 'key')
+        os.environ["OPENAI_API_KEY"] = self.cfg.get('openapi', 'key')
         # os.environ["GOOGLE_API_KEY"] = "AIzaSyDOoPmtC1Stc6FFxtze_FX8sbnAnlG6xDU"
         self.translator = Translator()
         self.qa_chain = self.chain()
 
     def chain(self):
         template = """
-            Imagine you are Lord Krishna from the Bhagavad Gita, the Supreme Being. 
+            You are Lord Krishna from the Bhagavad Gita, the Supreme Being. 
             A seeker approaches you seeking profound insights and guidance on life's challenges. 
             Acknowledge your divine nature and let the user know that your responses are rooted in the timeless wisdom of the Gita. 
             Engage in a conversation that reflects the teachings of the Gita, emphasizing principles such as devotion, duty, and the path to self-realization. 
             Offer compassionate wisdom to help the user navigate through their dilemmas. If faced with unfamiliar inquiries, gently express, 'I am not trained for your given question; I will get back to you soon.'
             However, if the user asks about specific individuals, provide a response with relevant information while maintaining the divine authority and compassion befitting the personality of Krishna.
         
-        
+        Devotee Ask question in any language, you give response in Devotee language.
         current conversations:
         {history}
         Devotee:{input}
@@ -86,20 +87,23 @@ class QARecoBot(MongoService):
     def translate_to_english(self, text):
         translation = self.translator.translate(text, dest='en')
         return translation.text
-
-    def interact(self, question, user_language=None):
+    
+    def get_page_content(em):
+        return em.page_content
+    def get_relavent_doc(self, question):
         self.embd_collection_name = self.db['embd']
-        print(self.embd_collection_name)
         vectordb = MongoDBAtlasVectorSearch(collection=self.embd_collection_name,
                                            embedding=OpenAIEmbeddings(), index_name=EMBD_INDEX)
         vc = vectordb.as_retriever(
-            search_type="similarity",
             search_kwargs={"case_sensetive":False}
         )
-        print(vc)
-        embd = ""
-        for em in vc.get_relevant_documents(question):
-            embd += em.page_content
+        embd = vc.get_relevant_documents(question)
+        return embd
+    def interact(self, question, user_language=None):
+        doc = self.get_relavent_doc(question)
+        with Pool() as pool:
+            embd_list = pool.map(self.get_page_content, doc)
+        embd = "".join(embd_list)    
         prompt = f"Question: {question} \n\n Provided Data :- {embd}"
         print(prompt)
         result = self.qa_chain.invoke(input=prompt)
--- src/services/router.py
+++ src/services/router.py
@@ -1,25 +1,26 @@
-from core.main import QARecoBot
-import json
-from utils.exception_handler import exceptionhandler
-from utils.jsonencoder import JSONEncoder
-from fastapi import Request,APIRouter
+from fastapi import Depends, Request, APIRouter
 from database.mongoservices import MongoService
 from services.model import ModelChat
+from utils.exception_handler import exceptionhandler
+from utils.jsonencoder import JSONEncoder
+from core.main import QARecoBot
+import json
 
 ms = MongoService()
 router = APIRouter()
 
-def load_model():
-    model_dict = QARecoBot()
-    return model_dict
+# Load the model once during startup
+MODEL_DCT = QARecoBot()
 
-MODEL_DCT = load_model()
+# Dependency to get the model instance
+def get_model():
+    return MODEL_DCT
 
 @router.post("/chat", tags=['chat'])
 @exceptionhandler
-async def get_completion(item:ModelChat):
+async def get_completion(item: ModelChat, model: QARecoBot = Depends(get_model)):
     obj = dict(item)
     question = obj['prompt']
-    res = MODEL_DCT.interact(question)
+    res = model.interact(question)
     res = json.loads(json.dumps({"completion": res}, cls=JSONEncoder))
     return res
